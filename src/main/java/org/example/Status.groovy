bb61d939ce030fff0eaf82cc67ba31ce50fba2f8
BeanZip <buntonbrondon2010@gmail.com>
2024-09-29 15:27:34 -0400

    Pain...and Suffering and GROOVY!

--- src/main/java/org/example/Status.groovy
+++ src/main/java/org/example/Status.groovy
@@ -5,9 +5,13 @@ import javax.swing.JOptionPane
 
 class Status{
     static def int check(IllegalArgumentException e){
-        JFrame frame = new JFrame("WINDOW ERROR!")
+        JFrame frame = new JFrame("Initialization Error Message!")
         frame.setSize(400,400)
         JOptionPane.showMessageDialog(frame,"Window Could Not Initialize" + e)
+        Thread.start{
+            Thread.sleep(2000)
+            frame.dispose()
+        }
         return 1
     }
 }
